8c0a395ae8226a25fc4366e69ac0a58477e8f187
WUTTIKAN <xirconsss@gmail.com>
2025-06-27 16:50:50 +0700

    host

--- server/models/db.js
+++ server/models/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'store.atelien'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
